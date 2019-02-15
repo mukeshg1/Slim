5e9991a9d8afa36c6c38dab70faae2feb140f16d
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-02-15 12:19:24 +0530

    Get users by id added.

--- src/php/server.php
+++ src/php/server.php
@@ -90,6 +90,49 @@ $app->post('/user/add', function(Request $request, Response $respose)
 });
 
 
+//Get a single user with id
+$app->get('/user/{recid}', function(Request $request, Response $respose, array $args)
+{
+
+	require_once ('C:\xampp\htdocs\slimapiCRUD\src\FileMakerCWP/FileMaker.php');
+
+	$layout_name = 'CRUD';
+
+	//New FileMaker Instantiation
+	$fm = new FileMaker();
+	$fm->setProperty('database', 'CRUD');
+	$fm->setProperty('hostspec', '172.16.9.42');
+	$fm->setProperty('username', 'admin');
+	$fm->setProperty('password', 'mindfire');
+
+
+	$recid = $args['recid'];
+    $record = $fm->getRecordById($layout_name, $recid);
+
+	$connected = $fm->listLayouts();
+
+	$layout_object = $fm->getLayout($layout_name);
+
+	$field_objects = $layout_object->getFields();
+
+	If(FileMaker::isError($connected)){
+	        echo "Error:".$connected;
+	        exit;
+	}
+	IF(FileMaker::isError($record)){
+		echo "Error, record doesn't exist.";
+		exit;
+	}
+	foreach ($field_objects as $field_object) {
+		$field_name = $field_object->getName();
+		$field_value = $record->getField($field_name);
+		$field_value = htmlspecialchars($field_value);
+		$field_value = nl2br($field_value);
+		//JSONSetElement("{$field_name:$field_valued}";JSONObject);
+		echo  $field_name.': '.$field_value.'<br>';
+		//$myJSON;
+	 }
+});
 
 
 
